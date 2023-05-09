7481ce72feee236d060659d25e88ec7034c83265
kimjjunho <kimjjun1007@gmail.com>
2023-05-09 16:15:18 +0900

    profile ui

--- app/lib/feature/mypage/MyPageScreen.dart
+++ app/lib/feature/mypage/MyPageScreen.dart
@@ -15,8 +15,21 @@ class MyPageScreen extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
+            SizedBox(height: 16),
             Header(title: '마이페이지'),
-            Body6(text: "")
+            SizedBox(height: 25),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(50),
+              child: Image.asset('assets/images/img_profile.png', width: 60, height: 60,),
+            ),
+            SizedBox(height: 16),
+            ProfileInfoText(title: "이름", content: ""),
+            SizedBox(height: 20),
+            ProfileInfoText(title: "생년월일", content: ""),
+            SizedBox(height: 20),
+            ProfileInfoText(title: "학년 반 번호", content: ""),
+            SizedBox(height: 20),
+            ProfileInfoText(title: "아이디", content: "")
           ],
         ),
       ),
@@ -31,20 +44,25 @@ class ProfileInfoText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 16),
-              child: Column(
-                children: [
-                  Body6(text: title)
-                ],
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Body6(text: title),
+          SizedBox(height: 8),
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 9, horizontal: 16),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              border: Border.all(
+                width: 1,
+                color: gray200
               ),
-            )
-          ],
-        ),
+            ),
+            child: Body4(text: content),
+          )
+        ],
       ),
     );
   }
