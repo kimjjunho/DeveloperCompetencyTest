70b3e0364db05734083973057896d8e105a3ed84
kimjjunho <kimjjun1007@gmail.com>
2023-05-16 12:25:43 +0900

    start2

---
+++ app2/design-system/Colors.js
@@ -0,0 +1,36 @@
+const Color = {
+
+  gray100:'#FDFDFD',
+  gray200:'#EDEDED',
+  gray300:'#D3D3D3',
+  gray400:'#BDBDBD',
+  gray500:'#A7A7A7',
+  gray600:'#929292',
+  gray700:'#7C7C7C',
+  gray800:'#505050',
+  gray900:'#343434',
+
+  green100:'#C6FFC8',
+  green200:'#C0F1C2',
+  green300:'#ACE4AE',
+  green400:'#89DA8C',
+  green500:'#04DF00',
+  green600:'#48A74B',
+  green700:'#257127',
+  green800:'#08440A',
+
+  blue100:'#C7D0FF',
+  blue200:'#9FAEF8',
+  blue300:'#637BF2',
+  blue400:'#006EFF',
+  blue500:'#045ACC',
+
+  red:'#DC3035',
+  yellow:'#FAF288',
+
+  white:'#fff',
+  black:'000'
+
+};
+
+export default Color;
