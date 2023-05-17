a397d52efc30f61921386fa4d821629d9ccd3fd4
kimjjunho <kimjjun1007@gmail.com>
2023-05-17 09:16:33 +0900

    add fonts

--- app2/App.js
+++ app2/App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Typography from './design-system/Typography';
+import { useFonts } from "expo-font";
 import Color from './design-system/Colors';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    'Bold' : require('./assets/font/NotoSans-Bold.ttf'),
+    'Regular' : require('./assets/font/NotoSans-Regular.ttf'),
+  })
+  if (!fontsLoaded) {
+    return null
+  }
+
   return (
     <View style={styles.container}>
-      <Text>working app</Text>
+      <Text style={[Typography.title1, { color:Color.blue300 }]}>working app</Text>
+      <Text style={[Typography.title2]}>working app</Text>
       <StatusBar style="auto" />
   </View>
   );
---
+++ app2/design-system/Typography.js
@@ -0,0 +1,61 @@
+import { useFonts } from "expo-font";
+
+const Bold = 'Bold';
+const Regular = 'Regular';
+
+const Typography = {
+  title1: {
+    fontSize: 24,
+    fontFamily: Bold
+  },
+  title2: {
+    fontSize: 20,
+    fontFamily: Bold
+  },
+  title3: {
+    fontSize: 20,
+    fontFamily: Regular
+  },
+  body1: {
+    fontSize: 18,
+    fontFamily: Bold
+  },
+  body2: {
+    fontSize: 18,
+    fontFamily: Regular
+  },
+  body3: {
+    fontSize:16,
+    fontFamily: Bold
+  },
+  body4: {
+    fontSize: 16,
+    fontFamily: Regular
+  },
+  Body5: {
+    fontSize: 14,
+    fontFamily: Bold
+  },
+  Body6: {
+    fontSize: 14,
+    fontFamily: Regular
+  },
+  detail1: {
+    fontSize: 12,
+    fontFamily: Bold
+  },
+  detail2: {
+    fontSize: 12,
+    fontFamily: Regular
+  },
+  detail3: {
+    fontSize: 10,
+    fontFamily: Bold
+  },
+  detail4: {
+    fontSize: 10,
+    fontFamily: Regular
+  }
+};
+
+export default Typography;
