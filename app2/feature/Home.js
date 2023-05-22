9e995f2d4633975a7975a1735602bb5ceb421c07
kimjjunho <kimjjun1007@gmail.com>
2023-05-23 00:15:26 +0900

    문제 아이템

--- app2/feature/Home.js
+++ app2/feature/Home.js
@@ -1,41 +1,53 @@
 import React from "react";
-import { Text, View, FlatList } from "react-native";
+import { Text, View, FlatList, Pressable, Alert } from "react-native";
 import Typography from "../design-system/Typography";
 import GHeader from "../design-system/component/Header";
 import Color from "../design-system/Colors";
 
 const HomeData = [
   {
-    title: '가위바위보'
+    title: '가위바위보',
+    color: Color.yellow,
+    count: 5,
   },
   {
-    title: '도형 회전하기'
+    title: '숫자 누르기',
+    color: Color.yellow,
+    count: 5,
   },
   {
-    title: '약속 정하기'
+    title: '도형 회전하기',
+    color: Color.green500,
+    count: 5,
   },
   {
-    title: '길 만들기'
+    title: '약속 정하기',
+    color: Color.green500,
+    count: 5,
   },
   {
-    title: '마법약 만들기'
+    title: '길 만들기',
+    color: Color.blue400,
+    count: 5,
   },
   {
-    title: '숫자 누르기'
+    title: '도형 순서 기억하기',
+    color: Color.blue400,
+    count: 5,
   },
   {
-    title: '도형 순서 기억하기'
+    title: '고양이 술래잡기',
+    color: Color.red,
+    count: 5,
   },
   {
-    title: '고양이 술래잡기'
+    title: '마법약 만들기',
+    color: Color.red,
+    count: 5,
   },
-  {
-    title: '개수 비교하기'
-  }
 ];
 
 export default function Home() {
-  
   return(
     <View style={{backgroundColor: Color.white, marginBottom: 60}}>
       <GHeader title={"홈"}/>
@@ -49,10 +61,30 @@ export default function Home() {
                 marginVertical: 7,
                 borderRadius: 5,
                 backgroundColor: Color.white,
-                elevation: 3
+                elevation: 3,
+                flexDirection: "row",
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                shadowColor: Color.gray500,
+                shadowOpacity: 1,
+                shadowOffset: {
+                  height: 0,
+                  width: 0
+                }
               }
             }>
-            <Text style={Typography.body3}>{item.title}</Text>
+              <View style={{marginStart: 30}}>
+                <Text style={[Typography.body3]}>{item.title}</Text>
+                <View style={{marginTop: 9, flexDirection: "row", alignItems: 'center'}}>
+                  <Pressable
+                    onPress={()=>Alert.alert('click')}>
+                    <Text style={[Typography.detail2, {color: Color.green400}]}>게임 </Text>
+                  </Pressable>
+                  <Text style={[Typography.detail2, {color: Color.gray600}]}>| {item.count}문제</Text>
+
+                </View>
+              </View>
+              <View style={{backgroundColor: item.color, height: 64, width: 64, borderRadius: 50, marginEnd: 30}}/>
           </View>
         }
       }
