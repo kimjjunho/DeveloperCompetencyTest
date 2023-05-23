7f6acbc448dc8639409fecbc67b5dff999a5b14e
kimjjunho <kimjjun1007@gmail.com>
2023-05-23 14:50:03 +0900

    record tabbar

--- app2/design-system/component/Header.js
+++ app2/design-system/component/Header.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { SafeAreaView, Text } from 'react-native';
 import Typography from '../Typography';
 
-function GHeader({ title }) {
+function GHeader({ title, color }) {
   return (
-    <SafeAreaView>
-      <Text style={[Typography.title2, {marginLeft:26}, {paddingVertical: 11}]}>{title}</Text>
+    <SafeAreaView style={{backgroundColor: color}}>
+      <Text style={[Typography.title2, {marginLeft:26, paddingVertical: 11}]}>{title}</Text>
     </SafeAreaView>
   );
 };
--- app2/feature/Record.js
+++ app2/feature/Record.js
@@ -1,11 +1,38 @@
 import React from "react";
 import { View } from "react-native";
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import Color from "../design-system/Colors";
 import GHeader from "../design-system/component/Header";
+
+const ScoreList = () => (
+  <View style={{flex: 1, backgroundColor: Color.green800}}></View>
+);
+
+const Graph = () => (
+  <View style={{flex: 1, backgroundColor: Color.black}}></View>
+);
+
+const Tab = createMaterialTopTabNavigator();
 
 export default function Record() {
   return(
-    <View>
-      <GHeader title={"기록"}/>
-    </View>
+    <>
+     <GHeader title={"기록"} color={Color.white}/>
+     <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: Color.green600,
+        tabBarInactiveTintColor: Color.gray600,
+        tabBarIndicatorStyle: {
+          backgroundColor: Color.green600,
+        },
+        tabBarLabelStyle: {
+          fontSize: 14,
+          fontFamily: 'Regular',
+        }
+      }}>
+      <Tab.Screen name="리스트" component={ScoreList}/>
+      <Tab.Screen name="그래프" component={Graph}/>
+    </Tab.Navigator>
+    </>
   );
 };
